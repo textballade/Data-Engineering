91cf58ee7e46769ab0be305c445a4b7ee94da5fa
textballade <textballade@gmail.com>
2026-04-24 21:33:37 +0000

    add producer/consumer mvp

---
+++ kafka-flint/src/producers/producer.py
@@ -0,0 +1,46 @@
+import dataclasses
+import json
+import sys
+import time
+from pathlib import Path
+
+# sys.path - список папок, в которых Python ищет файлы при import
+# insert(0, value) - вставить в начале списка
+# Path(__file__) - путь к текущему файлу
+# .parent.parent - в данном случае src (чтобы можно было добавить models)
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import pandas as pd
+from kafka import KafkaProducer
+from models import Ride, ride_from_row
+
+# Download NYC yellow taxi trip data (first 1000 rows)
+url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-11.parquet"
+columns = ['PULocationID', 'DOLocationID', 'trip_distance', 'total_amount', 'tpep_pickup_datetime']
+df = pd.read_parquet(url, columns=columns).head(1000)
+
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    json_str = json.dumps(ride_dict)
+    return json_str.encode('utf-8')
+
+server = 'localhost:9092'
+
+producer = KafkaProducer(
+    bootstrap_servers=[server],
+    value_serializer=ride_serializer
+)
+t0 = time.time()
+
+topic_name = 'rides'
+
+for _, row in df.iterrows():
+    ride = ride_from_row(row)
+    producer.send(topic_name, value=ride)
+    print(f"Sent: {ride}")
+    time.sleep(0.01)
+
+producer.flush()
+
+t1 = time.time()
+print(f'took {(t1 - t0):.2f} seconds')
